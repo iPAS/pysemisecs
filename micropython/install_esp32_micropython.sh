ec7c789b5eac1aeb4aff0ba45c68f6adcbc9a7f0
iPAS <ptiwatthanont@gmail.com>
2024-07-12 20:56:16 +0700

    update install_esp32_micropython.sh to be flashing relectable

--- micropython/install_esp32_micropython.sh
+++ micropython/install_esp32_micropython.sh
@@ -3,40 +3,57 @@
 port="/dev/ttyUSB0"
 [[ -n "$1" ]] && port="$1"  # -n means 'not empty', -z means 'empty'
 
-[[ -z "$2" ]] && echo "Please download from: https://micropython.org/download/?port=esp32" && exit 255
-firmware="$2"
-
 [[ -z "$(which esptool.py)" ]] && echo "Please: pip install esptool" && exit 255
 [[ -z "$(which mpremote)" ]] && echo "Please: pip install mpremote" && exit 255
 [[ -z "$(which mpy-cross)" ]] && echo "Please: find in micropython/mpy-cross" && exit 255
 
-echo
-echo '>>> Flash the firmware'
-esptool.py --chip esp32 --port ${port} erase_flash
-esptool.py --chip esp32 --port ${port} --baud 460800 write_flash -z 0x1000 ${firmware}
+if [ -z "$2" ]; then
+    echo 'Please download from: https://micropython.org/download/?port=esp32'
+    echo 'So, will not re-program the firmware.'
+else
+    firmware="$2"
+    echo
+    echo '>>> Flash the firmware'
+    esptool.py --chip esp32 --port ${port} erase_flash
+    esptool.py --chip esp32 --port ${port} --baud 460800 write_flash -z 0x1000 ${firmware}
+fi
 
-sleep 1
+sleep 3
 
 echo
 echo '>>> Install the libraries'
+required_mpy='yes'
 target_path=semi
 mp_cmd="mpremote connect ${port}"
 
 ${mp_cmd} fs mkdir semi 1>/dev/null 2>/dev/null
-# ${mp_cmd} fs cp lib/*.py :${target_path}/
-# ${mp_cmd} fs cp secs.py :${target_path}/
 
 cp secs.py lib/
-
 cd lib
+
+# Remove files on the target
+for f in *.py; do
+    ${mp_cmd} fs rm ":${target_path}/$f" 1>/dev/null 2>/dev/null
+done
+
+# Copy .py files to the target
+[[ -z "${required_mpy}" ]] && ${mp_cmd} fs cp *.py :${target_path}/
+
+# mpy-cross
 for f in *.py; do
     # mpy-cross -v -march=xtensawin -X emit=native -- $f
     mpy-cross -v -march=xtensawin -- $f
-    ${mp_cmd} fs rm ":${target_path}/$f" 1>/dev/null 2>/dev/null
 done
+
+# Copy .mpy files to the target
 for f in *.mpy; do
-    ${mp_cmd} fs cp $f ":${target_path}/"
+    ${mp_cmd} fs rm ":${target_path}/$f" 1>/dev/null 2>/dev/null
+    [[ -n "${required_mpy}" ]] && ${mp_cmd} fs cp $f ":${target_path}/"
     rm -f $f
 done
+
 rm -f secs.py
 cd ..
+
+${mp_cmd} fs ls :${target_path}/
+${mp_cmd} reset
